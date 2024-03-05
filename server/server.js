22919c81e7c06c92250fed231815f5c4b6f74754
Shiriatsu <siriusying716@gmail.com>
2024-03-05 18:36:33 -0500

    Modify the payment page

--- server/server.js
+++ server/server.js
@@ -1,89 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const bodyparser = require("body-parser");
+const checkoutRoutes = require('./checkout'); 
 
 const app = express();
+
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use(cors({ origin: true, credentials: true }));
 
-const stripe = require("stripe")("sk_test_51OBjICLeBQEWi3hthAiWaiu6V47yf2mxmw3YXT8qiX0hhzZkDxWsyAJqu3fvezmLofoOd561eSma8lgDZc2KAhs900KnTlmE87");
 
 const authRoutes = require('./authRoutes');
 app.use('/', authRoutes); 
 
-app.post("/checkout", async (req, res, next) => {
-    try {
-        const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        shipping_address_collection: {
-        allowed_countries: ['US', 'CA'],
-        },
-            shipping_options: [
-            {
-                shipping_rate_data: {
-                type: 'fixed_amount',
-                fixed_amount: {
-                    amount: 0,
-                    currency: 'usd',
-                },
-                display_name: 'Free shipping',
-                // Delivers between 5-7 business days
-                delivery_estimate: {
-                    minimum: {
-                    unit: 'business_day',
-                    value: 5,
-                    },
-                    maximum: {
-                    unit: 'business_day',
-                    value: 7,
-                    },
-                }
-                }
-            },
-            {
-                shipping_rate_data: {
-                type: 'fixed_amount',
-                fixed_amount: {
-                    amount: 1500,
-                    currency: 'usd',
-                },
-                display_name: 'Next day air',
-                // Delivers in exactly 1 business day
-                delivery_estimate: {
-                    minimum: {
-                    unit: 'business_day',
-                    value: 1,
-                    },
-                    maximum: {
-                    unit: 'business_day',
-                    value: 1,
-                    },
-                }
-                }
-            },
-            ],
-           line_items:  req.body.items.map((item) => ({
-            price_data: {
-              currency: 'usd',
-              product_data: {
-                name: item.name,
-                images: [item.product]
-              },
-              unit_amount: item.price * 100,
-            },
-            quantity: item.quantity,
-          })),
-           mode: "payment",
-           success_url: "http://localhost:4200/success.html",
-           cancel_url: "http://localhost:4200/cancel.html",
-        });
-
-        res.status(200).json(session);
-    } catch (error) {
-        next(error);
-    }
-});
+app.use('/checkout', checkoutRoutes);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
